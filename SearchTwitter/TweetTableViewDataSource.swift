755d563e744dc60115845bc136f15d1c85e52857
Isha <hereisisha@gmail.com>
2021-10-22 17:36:15 +0300

    Add Twitter json structure

---
+++ SearchTwitter/TweetTableViewDataSource.swift
@@ -0,0 +1,12 @@
+//
+//  TweetTableViewDataSource.swift
+//  SearchTwitter
+//
+//  Created by Isha Dua on 08/10/21.
+//
+
+import UIKit
+
+//class TweetTableViewDataSource: UITableViewDataSource {
+//    
+//}
